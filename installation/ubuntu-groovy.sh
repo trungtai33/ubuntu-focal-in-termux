e084241c4c73504f7b0acf21e933116f8d78ba9e
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-09-20 15:02:26 +0700

    official

--- installation/ubuntu-groovy.sh
+++ installation/ubuntu-groovy.sh
@@ -155,7 +155,7 @@ cat <<- EOF > "$PREFIX/share/$directory/proc/.model"
 $(getprop ro.product.brand) $(getprop ro.product.model)
 EOF
 cat <<- EOF > "$PREFIX/share/$directory/proc/.version"
-Linux version 5.8.0 (build@ubuntu) (gcc version 4.9 (GCC)) $(uname -v)
+Linux version 5.8.0 (termux@ubuntu) (gcc version 4.9 (GCC)) $(uname -v)
 EOF
 bin="start-ubuntu-groovy"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Writing $bin file...\n\e[0m"
