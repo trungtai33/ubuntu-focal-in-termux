48860c8d328498d3b97c8fbe32658d007dc39b13
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-09-23 18:55:19 +0700

    official

--- uninstall/ubuntu-groovy.sh
+++ uninstall/ubuntu-groovy.sh
@@ -3,7 +3,7 @@ if [ -d "$PREFIX/share/ubuntu-groovy" ]; then
 printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling Ubuntu Groovy, please wait...\n\e[31m"
 rm -rf "$PREFIX/share/ubuntu-groovy"
 rm -f "$PREFIX/bin/start-ubuntu-groovy"
-printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstalled successfully.\n\n\e[0m"
+printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
 printf "\n\e[31mError: distribution Ubuntu Groovy is not installed.\n\n\e[0m"
