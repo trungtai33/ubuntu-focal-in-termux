714f879ae9dc8829ef8d223fd481c8559aafa566
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-06 07:11:23 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+directory="ubuntu-groovy"
 distro_name="Ubuntu Groovy"
-if [ -d "${PREFIX}/share/ubuntu-groovy" ]; then
+if [ -d "${PREFIX}/share/${directory}" ]; then
 printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[31m"
-rm -rf "${PREFIX}/share/ubuntu-groovy"
-rm -f "${PREFIX}/bin/start-ubuntu-groovy"
+rm -rf "${PREFIX}/share/${directory}"
+rm -f "${PREFIX}/bin/start-${directory}"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
