0fc32acda39180a9e1b506077e3e641727c84e3f
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-14 15:58:19 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 directory="ubuntu-focal"
-distro_name="Ubuntu Focal"
+distribution="Ubuntu Focal"
 if [ -d "${PREFIX}/share/${directory}" ]; then
-printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[0m"
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distribution}, please wait...\n\e[0m"
 rm -rf "${PREFIX}/share/${directory}"
 rm -f "${PREFIX}/bin/start-${directory}"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
-printf "\n\e[31mError: distribution ${distro_name} is not installed.\n\n\e[0m"
+printf "\n\e[31mError: distribution ${distribution} is not installed.\n\n\e[0m"
